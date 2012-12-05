9a0c0127ab9cbd5b90c53197dd0eabf9ebcaee1e
Mike Lutton <michael.lutton@gmail.com>
2012-12-04 21:58:07 -0500

    initial project

---
+++ src/groovy/RomanNumeralConverter.groovy
@@ -0,0 +1,12 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mlutton
+ * Date: 12/4/12
+ * Time: 9:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class RomanNumeralConverter {
+    public String GetValue (int value) {
+
+    }}
---
+++ test/unit/RomanNumeralConverterTest.groovy
@@ -0,0 +1,22 @@
+import org.junit.Test
+/**
+ * Created with IntelliJ IDEA.
+ * User: mlutton
+ * Date: 12/4/12
+ * Time: 9:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class RomanNumeralConverterTest extends GroovyTestCase {
+
+    RomanNumeralConverter converter
+    String result
+
+    void setUp() {
+        converter = new RomanNumeralConverter()
+    }
+
+    void tearDown() {
+
+    }
+
+}
